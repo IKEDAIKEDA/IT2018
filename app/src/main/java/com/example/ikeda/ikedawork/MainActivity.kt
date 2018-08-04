b05c5b4ee3cea075ee7058db68b038f3fd9ddfd3
ikeda5272 <ikeda1990ikeda@gmail.com>
2018-08-05 00:33:39 +0900

    ・不要なボタンを削除 ・画像のクリックイベントを追加（が、落ちる。。）

--- app/src/main/java/com/example/ikeda/ikedawork/MainActivity.kt
+++ app/src/main/java/com/example/ikeda/ikedawork/MainActivity.kt
@@ -15,7 +15,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 画像表示、下移動、消える
         this.animateTranslationY(imageView1)
+        // クリックで消える
+        imageView1.setOnClickListener{
+            val animationList = mutableListOf<Animator>()
+
+            // ここで落ちる。。。
+            val objectAnimator0 = ObjectAnimator.ofFloat( imageView1, "resume")
+            animationList.add(objectAnimator0)
+
+            val objectAnimator1 = ObjectAnimator.ofFloat( imageView1, "alpha", 1f, 0f )
+            objectAnimator1.duration = 2000
+            animationList.add(objectAnimator1)
+
+            val animatorSet = AnimatorSet()
+            animatorSet.playSequentially(animationList)
+            animatorSet.start()
+            //objectAnimator.start()
+        }
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main,menu)
@@ -29,19 +47,22 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-    fun animateTranslationY(img: ImageView){
+    private fun animateTranslationY(img: ImageView){
         val animationList = mutableListOf<Animator>()
 
+        // 表示
         val objectAnimator0 = ObjectAnimator.ofFloat(img,"alpha",0f,1f)
         objectAnimator0.duration = 2000
         // objectAnimator0.repeatCount = -1
         animationList.add(objectAnimator0)
 
-        val objectAnimator1 = ObjectAnimator.ofFloat(img, "translationY",1000f)
-        objectAnimator1.duration = 2000
+        // Y軸（画面下方向）へ動く
+        val objectAnimator1 = ObjectAnimator.ofFloat(img, "translationY",2000f)
+        objectAnimator1.duration = 3000
         // objectAnimator.repeatCount = -1
         animationList.add(objectAnimator1)
 
+        // 透過
         val objectAnimator2 = ObjectAnimator.ofFloat(img,"alpha",1f,0f)
         objectAnimator2.duration = 2000
         animationList.add(objectAnimator2)
@@ -52,6 +73,13 @@ class MainActivity : AppCompatActivity() {
 
         //objectAnimator.start()
     }
+
+    private val onClickListener = { img:ImageView ->
+        val objectAnimator = ObjectAnimator.ofFloat( img, "alpha", 1f, 0f )
+        objectAnimator.duration = 2000
+        objectAnimator.start()
+    }
+
     fun changeTextView(view: View){
         messageTextView.text = "Hello, World"
     }
