62db6fc3f54aaa840917e53c4f6efc2ec92e928e
ikeda5272 <ikeda1990ikeda@gmail.com>
2018-08-10 02:11:59 +0900

    sounpoolで音がなるように実装・未確認

--- app/src/main/java/com/example/ikeda/ikedawork/MainActivity.kt
+++ app/src/main/java/com/example/ikeda/ikedawork/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.ikeda.ikedawork
 import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.media.MediaPlayer
+import android.media.SoundPool
+import android.media.AudioAttributes
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -15,8 +18,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //var soundPool:SoundPool? = null
+        var soundOne:Int = 0
+        val audioAttribute = AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH).build()
+        val soundPool = SoundPool.Builder().
+                setAudioAttributes(audioAttribute).build()
+        soundOne = soundPool.load(this, R.raw.water_drop3, 1)
+
         // 画像表示、下移動、消える
-        this.animateTranslationY(imageView1)
+        this.animateTranslationY(imageView1, soundPool, soundOne)
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main,menu)
@@ -30,8 +42,11 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-    private fun animateTranslationY(img: ImageView){
+    private fun animateTranslationY(img: ImageView, soundPool: SoundPool?,soundOne:Int){
         val animationList = mutableListOf<Animator>()
+        //val samlingrate = 44100
+        //var mp: MediaPlayer? = null
+        //mp = MediaPlayer.create(applicationContext, R.raw.water_drop3)
 
         // 表示
         val objectAnimator0 = ObjectAnimator.ofFloat(img,"alpha",0f,1f)
@@ -68,6 +83,11 @@ class MainActivity : AppCompatActivity() {
             animatorSetTouch.playSequentially(animationList)
             animatorSetTouch.start()
         }
+        soundPool?.play(soundOne,1.0f,1.0f,0,0,1.0f)
+        //mp?.start()
+        //mp?.reset()
+        //mp?.release()
+
     }
 
 /*    private val onClickListener = { img:ImageView ->
